0bade8b8bdb5d628a0fc990cff85e89adba5a326
Leon Rowland <leon@rowland.nl>
2013-10-14 15:01:51 +0200

    removed reference to old project

--- template.php
+++ template.php
@@ -31,7 +31,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 class SITE {
 	
 	/**
-	 * Holds the WP_KWPN plugin class
 	 * @var Pronamic_SITE_Plugin
 	 */
 	public $plugin;
